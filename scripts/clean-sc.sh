bc9125c3433f2042fc96fb0a97e5565aa3dac6bb
viktor_forsberg <viktor@elastisys.com>
2021-02-26 11:24:11 +0100

    fixes

--- scripts/clean-sc.sh
+++ scripts/clean-sc.sh
@@ -8,16 +8,16 @@ if [[ ${reply} != "y" ]]; then
     exit 1
 fi
 
-SCRIPTS_PATH="$(dirname "$(readlink -f "$0")")"
+here="$(dirname "$(readlink -f "$0")")"
 
-#Destroy all helm releases
-"${SCRIPTS_PATH}/.././bin/ck8s" ops helmfile sc destroy
+# Destroy all helm releases
+"${here}/.././bin/ck8s" ops helmfile sc destroy
 
-#Clean up namespaces and any other resources left behind by the apps
-"${SCRIPTS_PATH}/.././bin/ck8s" ops kubectl sc delete ns cert-manager dex elastic-system harbor fluentd influxdb-prometheus ingress-nginx monitoring velero
+# Clean up namespaces and any other resources left behind by the apps
+"${here}/.././bin/ck8s" ops kubectl sc delete ns cert-manager dex elastic-system harbor fluentd influxdb-prometheus ingress-nginx monitoring velero
 
-#Remove any lingering persistent volumes
-"${SCRIPTS_PATH}/.././bin/ck8s" ops kubectl sc delete pv --all
+# Remove any lingering persistent volumes
+"${here}/.././bin/ck8s" ops kubectl sc delete pv --all
 
-#Remove all added custom resource definitions
-"${SCRIPTS_PATH}/.././bin/ck8s" ops kubectl sc delete -f bootstrap/crds/ --recursive
+# Remove all added custom resource definitions
+"${here}/.././bin/ck8s" ops kubectl sc delete -f bootstrap/crds/ --recursive
